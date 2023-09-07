039d98e84612090e7a466304552b3e047eb4a533
Warda <hafeez.warda@gmail.com>
2023-09-07 12:45:49 -0400

    made another feature

---
+++ src/utils/Featuresss.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class Featuresss {
+    // this iss same as other features
+    // so report it as a bug and i can fix it
+    // so we dont have too many featuress in the class
+}
