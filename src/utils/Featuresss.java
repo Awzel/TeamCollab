23146c7a4ffcb8f8f341fdb4c93c500e65e8659e
Warda <hafeez.warda@gmail.com>
2023-09-07 12:47:57 -0400

    fixed spelling issues

--- src/utils/Featuresss.java
+++ src/utils/Featuresss.java
@@ -3,5 +3,5 @@ package utils;
 public class Featuresss {
     // this iss same as other features
     // so report it as a bug and i can fix it
-    // so we dont have too many featuress in the class
+    // so we don't have too many feature in the class
 }
