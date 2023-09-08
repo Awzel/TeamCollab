7a3b9cbba65d889ae957b6d1d12ea6a0c4a7880e
Awzel <awzal0515@gmail.com>
2023-09-07 20:33:29 -0400

    Updates

--- src/utils/Featuresss.java
+++ src/utils/Featuresss.java
@@ -4,4 +4,5 @@ public class Featuresss {
     // this iss same as other features
     // so report it as a bug and i can fix it
     // so we don't have too many feature in the class
+   // updates
 }
